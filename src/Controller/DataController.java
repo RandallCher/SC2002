e9028bf28650e019a75049ac762ebe0b17afc63e
RandallCher <100952571+RandallCher@users.noreply.github.com>
2022-10-31 09:48:04 +0800

    Add files via upload

---
+++ src/Controller/DataController.java
@@ -0,0 +1,22 @@
+public abstract class DataController implements Controller {
+
+	/**
+	 * 
+	 * @param fileName
+	 */
+	public object readFile(String fileName) {
+		// TODO - implement DataController.readFile
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param fileName
+	 * @param data
+	 */
+	public void writeFile(String fileName, Object data) {
+		// TODO - implement DataController.writeFile
+		throw new UnsupportedOperationException();
+	}
+
+}
