b06a4abfab1280d72bb4e986e7409ea3a6eb80de
weihaooooo <weihao2101@gmail.com>
2022-11-05 17:48:12 +0800

    booking, movielistingsview, payment , reviewview

--- src/View/MovieGoer/Booking.java
+++ src/View/MovieGoer/Booking.java
@@ -1,6 +1,13 @@
 package View.MovieGoer;
-
+import Controller.CineplexManager;
 import Model.*;
+import View.View;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+import static Controller.IOController.*;
+import static Controller.CineplexManager.*;
 
 public class Booking {
 
@@ -10,42 +17,99 @@ public class Booking {
 	private Seat seat;
 
 	/**
-	 * 
+	 *
 	 * @param seat
 	 */
 	public void BookingSeat(Seat seat) {
-		// TODO - implement Booking.BookingSeat
-		throw new UnsupportedOperationException();
+		this.seat = seat;
+		bookingFinished = false;
+		basePrice = seat.getShowtime().getCinema().getBasePrice();
+
 	}
 
 	protected void start() {
-		// TODO - implement Booking.start
-		throw new UnsupportedOperationException();
+		if (!bookingFinished){
+			displayMenu();
+		}
+		destroy();
 	}
 
 	private void displayMenu() {
-		// TODO - implement Booking.displayMenu
-		throw new UnsupportedOperationException();
+		System.out.println("Here are your booking details");
+		printBookingDetail();
+		System.out.println("1. Press 1 to enter your information");
+		System.out.println("2. Press 2 to go back");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		switch (input){
+			case 1: promptCustomerInformation();
+				break;
+			case 2: destroy();
+				break;
+		}
 	}
 
 	private void computeBasePrice() {
-		// TODO - implement Booking.computeBasePrice
-		throw new UnsupportedOperationException();
+		if(isWeekend(seat.getShowtime().getTime)){
+			ticketType = "Weekend ";
+			basePrice *= 1.5;
+
+		}
+		else{
+			ticketType = "Weekday ";
+		}
+		Holiday holiday = getHoliday(seat.getShowtime().getTime());
+		if (holiday != null){
+			double holidayRate = holiday.getRate();
+			basePrice *= holidayRate;
+			ticketType = ticketType + holiday.getName();
+
+		}
 	}
 
 	private void printBookingDetail() {
-		// TODO - implement Booking.printBookingDetail
-		throw new UnsupportedOperationException();
+		Showtime showtime = seat.getShowtime();
+		Movie movie = showtime.getMovie();
+		Cinema cinema = showtime.getCinema();
+
+		System.out.println(movie.getTitle());
+		System.out.println(movie.getAgeRestriction());
+		System.out.println("Date" + formatTimeMMddkkmm(showtime.getTime()));
+		System.out.println("Ticket type: " + ticketType);
+		System.out.println("Cinema: " + cinema + " (" + cinema.getCineplex() + ")");
+		System.out.println("Seat: Row " + (seat.getRow()+1) + " Col " + seat.getCol());
+		System.out.println("Ticket price (excluding GST): " + Math.round(basePrice));
 	}
 
 	private void promptCustomerInformation() {
-		// TODO - implement Booking.promptCustomerInformation
-		throw new UnsupportedOperationException();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name = sc.nextLine();
+		System.out.println("Enter your mobile number:");
+		String mobile = sc.nextLine();
+		System.out.println("Enter your Email address:");
+		String email = sc.nextLine();
+		System.out.println("Are you a senior citizen?: Y or N");
+		String input = sc.nextLine();
+		boolean isSeniorCitizen;
+		if (input == 'Y'){
+			isSeniorCitizen = true;
+		}
+		else{
+			isSeniorCitizen = false;
+		}
+		// Create customer object
+		Customer moviegoer = new Customer(name, mobile, email, isSeniorCitizen);
+
+		// proceed to payment
+		bookingFinished = true;
+		intent(this, new Payment(customer, seat, basePrice));
 	}
 
 	protected void destroy() {
-		// TODO - implement Booking.destroy
-		throw new UnsupportedOperationException();
+		((MovieListing)(prevView.prevView)).start(
+				CineplexManager.getMovieListing().get(CineplexManager.getMovieListing().indexOf(
+						seat.getShowtime().getMovie())));
 	}
 
 }
