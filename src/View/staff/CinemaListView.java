499d6656896419a65ade3de9b7c238a2c74a1141
phoeb02 <phoebe.leerouyin@gmail.com>
2022-11-06 19:34:59 +0800

    completed EditShowtimeView

---
+++ src/View/staff/CinemaListView.java
@@ -0,0 +1,21 @@
+package View.staff;
+
+import View.View;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Controller.CineplexController;
+import Model.Movie;
+import Model.Showtime;
+
+// might remove file (add cinema not necessary)
+public class CinemaListView extends View {
+
+	private boolean help;
+
+	public void start() {
+		// TODO
+	}
+
+}
